a335c2f3c44130cdf15b957a031930bdc65f615c
Ribbit <echo07200503@gmail.com>
2023-03-02 09:34:26 +0900

    ✨ プラグインに対応

--- src/event-listener.ts
+++ src/event-listener.ts
@@ -3,18 +3,20 @@ import { withMobileEvents } from './utilities';
 
 type ErrorHandler = (error: any, props: { event: kintoneAPI.Event }) => void;
 
-type ConstructorProps = { errorHandler?: ErrorHandler };
+type ConstructorProps = Partial<{ errorHandler: ErrorHandler; pluginId: string }>;
 
 export class KintoneEventListener {
+  readonly #pluginId?: string;
   readonly #commonErrorHandler: ErrorHandler;
 
   /**
    * 複数の処理を、各イベントに登録することができます
    */
   public constructor(props?: ConstructorProps) {
-    const { errorHandler = () => null } = props ?? {};
+    const { errorHandler = () => null, pluginId } = props ?? {};
 
     this.#commonErrorHandler = errorHandler;
+    this.#pluginId = pluginId;
   }
 
   /**
@@ -27,12 +29,15 @@ export class KintoneEventListener {
    */
   public add = <T = kintoneAPI.RecordData>(
     events: kintoneAPI.EventType[],
-    callback: (event: kintoneAPI.Event<T>) => kintoneAPI.Event<T> | Promise<kintoneAPI.Event<T>>
+    callback: (
+      event: kintoneAPI.Event<T>,
+      options?: { pluginId?: string }
+    ) => kintoneAPI.Event<T> | Promise<kintoneAPI.Event<T>>
   ) => {
     kintone.events.on(withMobileEvents(events), async (event) => {
       try {
         window.addEventListener('beforeunload', this.beforeunload);
-        return await callback(event);
+        return await callback(event, { pluginId: this.#pluginId });
       } catch (error) {
         await this.#commonErrorHandler(error, { event });
         throw error;
@@ -42,9 +47,7 @@ export class KintoneEventListener {
     });
   };
 
-  /**
-   * JavaScript中にページを離れようとした場合にアラートを表示します
-   */
+  /** JavaScript中にページを離れようとした場合にアラートを表示します */
   private beforeunload(event: BeforeUnloadEvent) {
     event.preventDefault();
     event.returnValue = '';
