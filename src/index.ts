124c6bd312b29dd70e275dc98cc3bf17b7be60cd
Ribbit <echo07200503@gmail.com>
2023-03-10 10:05:07 +0900

    ✨ dts-genの汎用的な型を追加

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,4 @@ export * from './character-width-conversion';
 export * from './event-listener';
 export * from './plugin';
 export * from './utilities';
+export * from './utility-types';
---
+++ src/utility-types.ts
@@ -0,0 +1,26 @@
+/** kintone.fieldTypes以下のプロパティをすべて参照する */
+export type SavedFields =
+  | kintone.fieldTypes.Calc
+  | kintone.fieldTypes.CheckBox
+  | kintone.fieldTypes.CreatedTime
+  | kintone.fieldTypes.Creator
+  | kintone.fieldTypes.Date
+  | kintone.fieldTypes.DateTime
+  | kintone.fieldTypes.DropDown
+  | kintone.fieldTypes.File
+  | kintone.fieldTypes.GroupSelect
+  | kintone.fieldTypes.Id
+  | kintone.fieldTypes.Link
+  | kintone.fieldTypes.Modifier
+  | kintone.fieldTypes.MultiLineText
+  | kintone.fieldTypes.MultiSelect
+  | kintone.fieldTypes.Number
+  | kintone.fieldTypes.OrganizationSelect
+  | kintone.fieldTypes.RadioButton
+  | kintone.fieldTypes.RecordNumber
+  | kintone.fieldTypes.Revision
+  | kintone.fieldTypes.RichText
+  | kintone.fieldTypes.SingleLineText
+  | kintone.fieldTypes.Time
+  | kintone.fieldTypes.UpdatedTime
+  | kintone.fieldTypes.UserSelect;
