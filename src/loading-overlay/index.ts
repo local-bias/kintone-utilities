922df113c91f95800f8cc79a6d5873d99be16b49
Ribbit <echo07200503@gmail.com>
2023-03-10 18:58:14 +0900

    🎨 デザインを修正

--- src/loading-overlay/index.ts
+++ src/loading-overlay/index.ts
@@ -99,12 +99,16 @@ export class LoadingOverlay {
           display: flex;
           flex-direction: column;
           align-items: center;
+          justify-content: center;
           gap: 16px;
           padding: 32px 64px;
           background-color: #fffc;
           border: 1px solid #fff;
           border-radius: 8px;
           box-shadow: 0 5px 24px -6px #0002;
+          min-width: 300px;
+          max-width: 90vw;
+          min-height: 200px;
         }
 
         .loader {
