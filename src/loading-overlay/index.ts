1514c00b92170799691c89c8fa23c8cda6d8e4bf
Ribbit <echo07200503@gmail.com>
2023-03-11 14:22:37 +0900

    👍 オーバーレイ内の各要素をそれぞれ管理

--- src/loading-overlay/index.ts
+++ src/loading-overlay/index.ts
@@ -2,15 +2,16 @@ import { css } from '@emotion/css';
 
 type ConstructorProps = Readonly<Partial<{ label: string; progress: number }>>;
 
-const ROOT_ID = '__🐸__loading-overlay';
-
 export class LoadingOverlay {
   #shown: boolean;
   #label: string | string[];
   #html: string;
   #progress: number | null;
+
   #root: HTMLDivElement;
-  #variable: HTMLDivElement;
+  #loaderElement: HTMLDivElement;
+  #progressElement: HTMLDivElement;
+  #contentElement: HTMLDivElement;
 
   public constructor(props: ConstructorProps = {}) {
     this.#shown = false;
@@ -18,34 +19,44 @@ export class LoadingOverlay {
     this.#html = '';
     this.#progress = props.progress ?? null;
 
-    this.addStyle();
-
-    const root =
-      document.querySelector<HTMLDivElement>(`#${ROOT_ID}`) || document.createElement('div');
-    root.id = ROOT_ID;
+    const root = document.createElement('div');
     this.#root = root;
     document.body.append(root);
 
     const container = document.createElement('div');
+    container.classList.add('__c');
     this.#root.append(container);
-    container.innerHTML = `<div class="loader"></div>`;
 
-    const variable = document.createElement('div');
-    container.append(variable);
-    this.#variable = variable;
+    const loaderElement = document.createElement('div');
+    loaderElement.classList.add('__l');
+    this.#loaderElement = loaderElement;
+    container.append(loaderElement);
 
+    const progressElement = document.createElement('div');
+    progressElement.classList.add('__p');
+    this.#progressElement = progressElement;
+    container.append(progressElement);
+
+    const contentElement = document.createElement('div');
+    this.#contentElement = contentElement;
+    container.append(contentElement);
+
+    this.addStyle();
     this.render();
   }
 
-  public start(): void {
+  public show(): void {
     this.#shown = true;
+    this.#loaderElement.style.animationName = 'spin';
     window.removeEventListener('beforeunload', this.beforeunload);
     document.body.style.overflow = 'hidden';
     this.render();
   }
 
-  public stop(): void {
+  public hide(): void {
     this.#shown = false;
+    this.#progress = 0;
+    this.#loaderElement.style.animationName = '';
     window.removeEventListener('beforeunload', this.beforeunload);
     document.body.style.overflow = 'auto';
     this.render();
@@ -62,21 +73,18 @@ export class LoadingOverlay {
   }
 
   private render(): void {
-    if (this.#shown) {
-      if (this.#html) {
-        this.#variable.innerHTML = `<div>${this.#label}</div><div class="progress" style="width: ${
-          this.#progress
-        }%"></div>`;
+    this.#root.style.opacity = this.#shown ? '1' : '0';
+    this.#root.style.pointerEvents = this.#shown ? 'all' : 'none';
+
+    this.#progressElement.style.width = `${this.#progress}%`;
+
+    if (this.#html) {
+      this.#contentElement.innerHTML = this.#html;
+    } else {
+      if (this.#label instanceof Array) {
+        this.#contentElement.innerHTML = `<div>${this.#label.join('</div><div>')}</div>`;
       } else {
-        if (this.#label instanceof Array) {
-          this.#variable.innerHTML = `<div>${this.#label.join(
-            '<br>'
-          )}</div><div class="progress" style="width: ${this.#progress}%"></div>`;
-        } else {
-          this.#variable.innerHTML = `<div>${
-            this.#label
-          }</div><div class="progress" style="width: ${this.#progress}%"></div>`;
-        }
+        this.#contentElement.innerText = this.#label;
       }
     }
   }
@@ -88,102 +96,85 @@ export class LoadingOverlay {
   }
 
   private addStyle() {
-    document.body.classList.add(css`
-      #${ROOT_ID} {
-        font-family: 'Yu Gothic Medium', '游ゴシック', YuGothic, 'メイリオ',
-          'Hiragino Kaku Gothic ProN', Meiryo, sans-serif;
-        color: #356;
-        font-size: 17px;
+    this.#root.classList.add(css`
+      font-family: 'Yu Gothic Medium', '游ゴシック', YuGothic, 'メイリオ',
+        'Hiragino Kaku Gothic ProN', Meiryo, sans-serif;
+      color: #356;
+      font-size: 17px;
 
+      overflow: hidden;
+      background-color: #fff6;
+      backdrop-filter: blur(10px);
+      box-sizing: content-box;
+
+      position: fixed;
+      inset: 0;
+      width: 100vw;
+      height: 100vh;
+
+      display: grid;
+      place-items: center;
+      transition: all 250ms ease;
+      z-index: 1000;
+      opacity: 0;
+      transition: all 250ms ease;
+
+      .__c {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        gap: 32px;
+        padding: 32px 64px;
+        background-color: #fffc;
+        border-radius: 8px;
+        box-shadow: 0 5px 24px -6px #0002;
+        min-width: 300px;
+        max-width: 90vw;
+        min-height: 200px;
+        position: relative;
         overflow: hidden;
-        background-color: #fff6;
-        backdrop-filter: blur(10px);
-        box-sizing: content-box;
+      }
 
-        position: fixed;
-        inset: 0;
-        width: 100vw;
-        height: 100vh;
+      .__l {
+        font-size: 48px;
+        width: 1em;
+        height: 1em;
+        position: relative;
+        border: 2px solid #2563ebaa;
+        animation: spin 2s infinite ease;
+      }
 
-        display: grid;
-        place-items: center;
+      .__p {
+        position: absolute;
+        bottom: 0px;
+        left: 0;
+        width: 0%;
+        height: 3px;
+        background-color: #2563eb;
         transition: all 250ms ease;
-        z-index: 1000;
+      }
 
-        @keyframes appear {
-          from {
-            opacity: 0;
-          }
-          to {
-            opacity: 1;
-          }
+      @keyframes spin {
+        0% {
+          transform: rotate(0deg);
+          border-radius: 1em;
         }
-
-        > div {
-          display: flex;
-          flex-direction: column;
-          align-items: center;
-          justify-content: center;
-          gap: 32px;
-          padding: 32px 64px;
-          background-color: #fffc;
-          border-radius: 8px;
-          box-shadow: 0 5px 24px -6px #0002;
-          min-width: 300px;
-          max-width: 90vw;
-          min-height: 200px;
-          position: relative;
-          overflow: hidden;
+        20% {
+          transform: rotate(0deg);
         }
-
-        .loader {
-          font-size: 48px;
-          width: 1em;
-          height: 1em;
-          position: relative;
-          border: 2px solid #2563ebaa;
-          animation: spin 2s infinite ease;
+        30%,
+        60% {
+          border-radius: 0.25em;
         }
-
-        @keyframes spin {
-          0% {
-            transform: rotate(0deg);
-            border-radius: 1em;
-          }
-          20% {
-            transform: rotate(0deg);
-          }
-          30%,
-          60% {
-            border-radius: 0.25em;
-          }
-          70% {
-            transform: rotate(180deg);
-          }
-          100% {
-            transform: rotate(180deg);
-            border-radius: 1em;
-          }
+        70% {
+          transform: rotate(180deg);
         }
-
-        .progress {
-          position: absolute;
-          bottom: 0px;
-          left: 0;
-          width: 60%;
-          height: 3px;
-          background-color: #2563eb;
-          transition: all 250ms ease;
+        100% {
+          transform: rotate(180deg);
+          border-radius: 1em;
         }
       }
     `);
   }
 }
-
-/**
- *       ${({ shown }) => (shown ? 'animation: appear 150ms ease 0s 1 forwards;' : '')};
-      pointer-events: ${({ shown }) => (shown ? 'initial' : 'none')};
- */
-
-/** ロード画面操作用クラスのシングルトン */
-export const loadingOverlay = new LoadingOverlay();
