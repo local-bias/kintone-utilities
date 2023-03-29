5217cba139804c5ad1ec348054d4fa1799f600bc
Ribbit <echo07200503@gmail.com>
2023-03-30 08:20:05 +0900

    ✨ 登録されたイベントの開始時と終了時にログを出力

--- src/event-listener.ts
+++ src/event-listener.ts
@@ -3,20 +3,27 @@ import { withMobileEvents } from './utilities';
 
 type ErrorHandler = (error: any, props: { event: kintoneAPI.Event }) => void;
 
-type ConstructorProps = Partial<{ errorHandler: ErrorHandler; pluginId: string }>;
+type ConstructorProps = Partial<{
+  errorHandler: ErrorHandler;
+  pluginId: string;
+  logDisabled: boolean;
+}>;
 
 export class KintoneEventListener {
+  readonly #uid: string = Math.random().toString(36).slice(2);
   readonly #pluginId?: string;
   readonly #commonErrorHandler: ErrorHandler;
+  #logDisabled: boolean;
 
   /**
    * 複数の処理を、各イベントに登録することができます
    */
   public constructor(props?: ConstructorProps) {
-    const { errorHandler = () => null, pluginId } = props ?? {};
+    const { errorHandler = () => null, pluginId, logDisabled = false } = props ?? {};
 
     this.#commonErrorHandler = errorHandler;
     this.#pluginId = pluginId;
+    this.#logDisabled = logDisabled;
   }
 
   /**
@@ -37,15 +44,55 @@ export class KintoneEventListener {
     kintone.events.on(withMobileEvents(events), async (event) => {
       try {
         window.addEventListener('beforeunload', this.beforeunload);
+        if (!this.#logDisabled) {
+          console.group(`%c${event.type}%c(${this.#uid})`, 'color: #1e40af;', 'color: #999');
+        }
         return await callback(event, { pluginId: this.#pluginId });
       } catch (error) {
         await this.#commonErrorHandler(error, { event });
         throw error;
       } finally {
         window.removeEventListener('beforeunload', this.beforeunload);
+        if (!this.#logDisabled) {
+          console.groupEnd();
+        }
       }
     });
   };
+
+  /**
+   * Changeイベントを登録します
+   *
+   * ChangeイベントはPromiseオブジェクトを返却することができません
+   * @param events
+   * @param callback
+   */
+  public addChangeEvents = <T = kintoneAPI.RecordData>(
+    events: string[],
+    callback: (event: kintoneAPI.Event<T>, options?: { pluginId?: string }) => kintoneAPI.Event<T>
+  ) => {
+    kintone.events.on(withMobileEvents(events), (event) => {
+      try {
+        window.addEventListener('beforeunload', this.beforeunload);
+        if (!this.#logDisabled) {
+          console.group(`%c${event.type}%c(${this.#uid})`, 'color: #1e40af;', 'color: #999');
+        }
+        return callback(event, { pluginId: this.#pluginId });
+      } catch (error) {
+        this.#commonErrorHandler(error, { event });
+        throw error;
+      } finally {
+        window.removeEventListener('beforeunload', this.beforeunload);
+        if (!this.#logDisabled) {
+          console.groupEnd();
+        }
+      }
+    });
+  };
+
+  public set logDisabled(value: boolean) {
+    this.#logDisabled = value;
+  }
 
   /** JavaScript中にページを離れようとした場合にアラートを表示します */
   private beforeunload(event: BeforeUnloadEvent) {
