e66a9605288c04992600b441c6d842c98173a149
Ribbit <echo07200503@gmail.com>
2023-04-07 15:49:44 +0900

    👍 APIの方をjs APIとREST APIで分割

--- src/api-types.ts
+++ src/api-types.ts
@@ -224,73 +224,85 @@ export declare namespace kintoneAPI {
     type App = { readonly app?: DefaultApp; readonly fields?: FieldProperties };
   }
 
-  type EventType =
-    | 'portal.show'
-    | 'app.record.index.show'
-    | 'app.record.index.edit.show'
-    | 'app.record.index.edit.submit'
-    | 'app.record.index.edit.submit.success'
-    | 'app.record.index.delete.submit'
-    | 'app.record.detail.show'
-    | 'app.record.detail.delete.submit'
-    | 'app.record.detail.process.proceed'
-    | 'app.record.create.show'
-    | 'app.record.create.change'
-    | 'app.record.create.submit'
-    | 'app.record.create.submit.success'
-    | 'app.record.edit.show'
-    | 'app.record.edit.change'
-    | 'app.record.edit.submit'
-    | 'app.record.edit.submit.success'
-    | 'app.record.print.show'
-    | 'mobile.app.record.index.show'
-    | 'mobile.app.record.detail.show'
-    | 'mobile.app.record.detail.delete.submit'
-    | 'mobile.app.record.detail.process.proceed'
-    | 'mobile.app.record.create.show'
-    | 'mobile.app.record.create.change'
-    | 'mobile.app.record.create.submit'
-    | 'mobile.app.record.create.submit.success'
-    | 'mobile.app.record.edit.show'
-    | 'mobile.app.record.edit.change'
-    | 'mobile.app.record.edit.submit'
-    | 'mobile.app.record.edit.submit.success';
+  /**
+   * @deprecated Use `kintoneAPI.js.EventType` instead.
+   */
+  type EventType = kintoneAPI.js.EventType;
 
-  type Event<T = RecordData> = {
-    appId: number;
-    viewId: number;
-    recordId: number;
-    record: T;
-    error: string;
-    url: string;
-    type: EventType;
-    changes?: {
-      field: {
-        type: string;
-        value: string;
+  /**
+   * @deprecated Use `kintoneAPI.js.Event` instead.
+   */
+  type Event<T = RecordData> = kintoneAPI.js.Event<T>;
+
+  namespace js {
+    type Event<T = RecordData> = {
+      appId: number;
+      viewId: number;
+      recordId: number;
+      record: T;
+      error: string;
+      url: string;
+      type: EventType;
+      changes?: {
+        field: {
+          type: string;
+          value: string;
+        };
+        row: any;
       };
-      row: any;
+      records?: T[];
+      viewType?: string;
+      viewName?: string;
+      offset?: number;
+      /**
+       * プロセス - 実行されたアクション
+       * app.record.detail.process.proceed イベントで取得できます
+       */
+      action?: { value: string };
+
+      /**
+       * プロセス - 現在のステータス
+       * app.record.detail.process.proceed イベントで取得できます
+       */
+      status?: { value: string };
+
+      /**
+       * プロセス - 次のステータス
+       * app.record.detail.process.proceed イベントで取得できます
+       */
+      nextStatus?: { value: string };
     };
-    records?: T[];
-    viewType?: string;
-    viewName?: string;
-    offset?: number;
-    /**
-     * プロセス - 実行されたアクション
-     * app.record.detail.process.proceed イベントで取得できます
-     */
-    action?: { value: string };
-
-    /**
-     * プロセス - 現在のステータス
-     * app.record.detail.process.proceed イベントで取得できます
-     */
-    status?: { value: string };
-
-    /**
-     * プロセス - 次のステータス
-     * app.record.detail.process.proceed イベントで取得できます
-     */
-    nextStatus?: { value: string };
-  };
+    type EventType =
+      | 'portal.show'
+      | 'app.record.index.show'
+      | 'app.record.index.edit.show'
+      | 'app.record.index.edit.submit'
+      | 'app.record.index.edit.submit.success'
+      | 'app.record.index.delete.submit'
+      | 'app.record.detail.show'
+      | 'app.record.detail.delete.submit'
+      | 'app.record.detail.process.proceed'
+      | 'app.record.create.show'
+      | 'app.record.create.change'
+      | 'app.record.create.submit'
+      | 'app.record.create.submit.success'
+      | 'app.record.edit.show'
+      | 'app.record.edit.change'
+      | 'app.record.edit.submit'
+      | 'app.record.edit.submit.success'
+      | 'app.record.print.show'
+      | 'mobile.app.record.index.show'
+      | 'mobile.app.record.detail.show'
+      | 'mobile.app.record.detail.delete.submit'
+      | 'mobile.app.record.detail.process.proceed'
+      | 'mobile.app.record.create.show'
+      | 'mobile.app.record.create.change'
+      | 'mobile.app.record.create.submit'
+      | 'mobile.app.record.create.submit.success'
+      | 'mobile.app.record.edit.show'
+      | 'mobile.app.record.edit.change'
+      | 'mobile.app.record.edit.submit'
+      | 'mobile.app.record.edit.submit.success';
+  }
+  namespace rest {}
 }
