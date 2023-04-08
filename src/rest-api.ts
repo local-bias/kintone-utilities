a075b8d010028d5e48f7b200f4cf827ed71adff8
Ribbit <echo07200503@gmail.com>
2023-04-09 07:24:52 +0900

    ✨ 複数のレコードを取得する関数を追加

--- src/rest-api.ts
+++ src/rest-api.ts
@@ -1,15 +1,34 @@
 import { kintoneAPI } from './api-types';
 
-const CHUNK_SIZE = 500;
+type App = number | string;
+const API_ENDPOINT_ROOT = '/k/v1';
+const API_ENDPOINT_CURSOR = `${API_ENDPOINT_ROOT}/records/cursor`;
+const API_LIMIT = {
+  GET: 500,
+};
+
+export const getAllRecords = async <T extends Record<string, any> = kintoneAPI.RecordData>(props: {
+  app: App;
+  query?: string;
+  fields?: string[];
+  onStep?: OnStep<T>;
+}) => {
+  if (props.query && props.query.includes('order by')) {
+    return getAllRecordsWithCursor<T>(props);
+  }
+  return getAllRecordsWithId<T>({ ...props, condition: props.query });
+};
 
 type WithId<T> = T & { $id: kintoneAPI.field.ID };
+type OnStep<T> = (records: T[]) => void;
 
 export const getAllRecordsWithId = async <T extends Record<string, any>>(props: {
-  app: number | string;
+  app: App;
   fields?: string[];
-  onStep?: (current: T[]) => void;
+  onStep?: OnStep<T>;
   condition?: string;
 }): Promise<WithId<T>[]> => {
+  checkBrowser();
   const { fields: initFields, condition: initCondition = '' } = props;
 
   const fields = initFields?.length ? [...new Set([...initFields, '$id'])] : undefined;
@@ -21,10 +40,10 @@ export const getAllRecordsWithId = async <T extends Record<string, any>>(props: 
 };
 
 const getRecursive = async <T extends Record<string, unknown>>(props: {
-  app: number | string;
+  app: App;
   fields: (keyof T)[] | undefined;
   condition: string;
-  onStep?: (current: T[]) => void;
+  onStep?: OnStep<T>;
   id?: string;
   stored?: WithId<T>[];
 }): Promise<WithId<T>[]> => {
@@ -32,13 +51,17 @@ const getRecursive = async <T extends Record<string, unknown>>(props: {
 
   const newCondition = id ? `${condition ? `${condition} and ` : ''} $id < ${id}` : condition;
 
-  const query = `${newCondition} order by $id desc limit ${CHUNK_SIZE}`;
+  const query = `${newCondition} order by $id desc limit ${API_LIMIT.GET}`;
 
-  const { records } = await kintone.api(kintone.api.url('/k/v1/records', true), 'GET', {
-    app,
-    fields,
-    query,
-  });
+  const { records } = await kintone.api(
+    kintone.api.url(`${API_ENDPOINT_ROOT}/records`, true),
+    'GET',
+    {
+      app,
+      fields,
+      query,
+    }
+  );
   if (!records.length) {
     return props.stored ?? [];
   }
@@ -52,5 +75,72 @@ const getRecursive = async <T extends Record<string, unknown>>(props: {
   const lastRecord = stored[stored.length - 1];
   const lastId = lastRecord.$id.value;
 
-  return records.length === CHUNK_SIZE ? getRecursive({ ...props, id: lastId, stored }) : stored;
+  return records.length === API_LIMIT.GET ? getRecursive({ ...props, id: lastId, stored }) : stored;
+};
+
+type OnTotalGet = (total: number) => void;
+
+export const getAllRecordsWithCursor = async <T extends Record<string, any>>(props: {
+  app: App;
+  fields?: string[];
+  query?: string;
+  onTotalGet?: OnTotalGet;
+  onStep?: OnStep<T>;
+}): Promise<T[]> => {
+  checkBrowser();
+  const { app, fields = [], query = '', onTotalGet = null, onStep = null } = props;
+
+  const param = { app, fields, size: API_LIMIT.GET, query };
+
+  const cursor = await kintone.api(kintone.api.url(API_ENDPOINT_CURSOR, true), 'POST', param);
+
+  if (onTotalGet) {
+    onTotalGet(cursor.totalCount);
+  }
+
+  return getRecordsByCursorId<T>({ id: cursor.id, onStep });
+};
+
+const getRecordsByCursorId = async <T>(props: {
+  id: string;
+  onStep: OnStep<T> | null;
+  loadedData?: T[];
+}): Promise<T[]> => {
+  const { id, onStep, loadedData = [] } = props;
+  const response = await kintone.api(kintone.api.url(API_ENDPOINT_CURSOR, true), 'GET', {
+    id,
+  });
+
+  const newRecords: T[] = [...loadedData, ...(response.records as T[])];
+
+  if (onStep) {
+    onStep(newRecords);
+  }
+
+  return response.next ? getRecordsByCursorId({ id, onStep, loadedData: newRecords }) : newRecords;
+};
+
+export const uploadFile = async (props: {
+  file: { name: string; data: Blob };
+}): Promise<{ fileKey: string }> => {
+  checkBrowser();
+  const { file } = props;
+
+  const formData = new FormData();
+  formData.append('__REQUEST_TOKEN__', kintone.getRequestToken());
+  formData.append('file', file.data, file.name);
+
+  const headers = { 'X-Requested-With': 'XMLHttpRequest' };
+  const response = await fetch('/k/v1/file.json', {
+    method: 'POST',
+    headers,
+    body: formData,
+  });
+  return response.json();
+};
+
+const checkBrowser = () => {
+  if (typeof window === 'undefined') {
+    throw new Error('この関数はブラウザでのみ使用できます');
+  }
 };
