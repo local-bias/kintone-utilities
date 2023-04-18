23659f8538e06c47a976a6a9d3dd25dada7a03fb
ribbit <echo07200503@gmail.com>
2023-04-18 11:35:30 +0900

    👍 APIの宛先が異なるため、cybozu USER APIを独立

--- src/rest-api/cybozu-user.ts
+++ src/rest-api/cybozu-user.ts
@@ -1,17 +1,80 @@
 import { kintoneAPI } from '../types/api';
-import { api } from './common';
 
-const API_ENDPOINT_ROOT = '/v1';
-const API_ENDPOINT_USERS = `${API_ENDPOINT_ROOT}/users.json`;
-const API_ENDPOINT_SERVICES = `${API_ENDPOINT_ROOT}/users/services.json`;
-const API_ENDPOINT_GROUPS = `${API_ENDPOINT_ROOT}/groups.json`;
+const API_ENDPOINT_USERS = 'users';
+const API_ENDPOINT_SERVICES = 'users/services';
+const API_ENDPOINT_GROUPS = 'groups';
 
-export const getCybozuUsers = (props: kintoneAPI.cybozu.GetUsersRequest = {}) => {
-  return api<kintoneAPI.cybozu.GetUsersResponse>(API_ENDPOINT_USERS, 'GET', props);
+const buildPath = (params: { endpointName: string }) => {
+  const { endpointName } = params;
+  return `/v1/${endpointName}.json`;
 };
-export const getUsedCybozuServices = (props: kintoneAPI.cybozu.GetUsedServicesRequest) => {
-  return api<kintoneAPI.cybozu.GetUsedServicesResponse>(API_ENDPOINT_SERVICES, 'GET', props);
+
+const api = async <T = any>(params: {
+  endpointName: string;
+  method: kintoneAPI.rest.Method;
+  body: any;
+  debug?: boolean;
+}): Promise<T> => {
+  const { endpointName, method, body, debug } = params;
+  try {
+    const path = buildPath({ endpointName });
+    if (debug) {
+      console.groupCollapsed(
+        `%ccybozu USER API %c(${endpointName})`,
+        'color: #1e40af;',
+        'color: #aaa'
+      );
+      console.log(`path: ${path}`);
+      console.log(`method: ${method}`);
+      console.log('body', body);
+    }
+    const response: T = await kintone.api(path, method, body);
+    if (debug) {
+      console.log('response', response);
+    }
+    return response;
+  } catch (error) {
+    if (debug) {
+      console.error(error);
+    }
+    throw error;
+  } finally {
+    if (debug) {
+      console.groupEnd();
+    }
+  }
 };
-export const updateUsedCybozuServices = (props: kintoneAPI.cybozu.UpdateUsedServicesRequest) => {
-  return api<kintoneAPI.cybozu.UpdateUsedServicesResponse>(API_ENDPOINT_SERVICES, 'PUT', props);
+
+export const getCybozuUsers = (
+  props: kintoneAPI.cybozu.GetUsersRequest & { debug?: boolean } = {}
+) => {
+  const { debug = false, ...body } = props;
+  return api<kintoneAPI.cybozu.GetUsersResponse>({
+    endpointName: API_ENDPOINT_USERS,
+    method: 'GET',
+    body,
+    debug,
+  });
+};
+export const getUsedCybozuServices = (
+  props: kintoneAPI.cybozu.GetUsedServicesRequest & { debug?: boolean }
+) => {
+  const { debug = false, ...body } = props;
+  return api<kintoneAPI.cybozu.GetUsedServicesResponse>({
+    endpointName: API_ENDPOINT_SERVICES,
+    method: 'GET',
+    body,
+    debug,
+  });
+};
+export const updateUsedCybozuServices = (
+  props: kintoneAPI.cybozu.UpdateUsedServicesRequest & { debug?: boolean }
+) => {
+  const { debug = false, ...body } = props;
+  return api<kintoneAPI.cybozu.UpdateUsedServicesResponse>({
+    endpointName: API_ENDPOINT_SERVICES,
+    method: 'PUT',
+    body,
+    debug,
+  });
 };
--- src/rest-api/index.ts
+++ src/rest-api/index.ts
@@ -1,3 +1,4 @@
+export * from './cybozu-user';
 export * from './record';
 export * from './record-comment';
 export * from './space';
