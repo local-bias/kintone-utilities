1d04e2d1cafc98b8b4f7ad520ad699c7a6ca1578
ribbit <echo07200503@gmail.com>
2023-04-20 15:26:55 +0900

    ✨ 開発環境と本番環境でドメインが異なるパターンに対応

--- src/types/kintone.config.ts
+++ src/types/kintone.config.ts
@@ -1,4 +1,5 @@
 export type KintoneConfig = {
+  baseUrl?: { dev: string; prod: string };
   apps: { dev: number; prod: number; name: string }[];
   kvs?: Record<string, { dev: string; prod: string }>;
 };
