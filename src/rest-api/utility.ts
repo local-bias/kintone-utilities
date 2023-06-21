dfdd3c9de2c2a4b82dcfbdc3580e357bd7db905c
Ribbit <echo07200503@gmail.com>
2023-06-21 18:52:19 +0900

    ✨ ユニークIDを生成する関数を追加

--- src/rest-api/utility.ts
+++ src/rest-api/utility.ts
@@ -20,3 +20,31 @@ export const filterFieldProperties = (
 
   return reduced;
 };
+
+/**
+ * ユニークなIDを生成します
+ * フォーマットはxxxxxxxx-xxxx-Cxxx-yxxx-xxxxxxxxxxxxです
+ *
+ * @param versionNumber バージョン番号(この数値を指定することで、バージョン間でIDの重複を防ぐことができます)
+ *
+ * @see {@link https://github.com/GoogleChrome/chrome-platform-analytics/blob/master/src/internal/identifier.js}
+ */
+export const createUUID = (versionNumber = '4') => {
+  const idTemplateArray = 'xxxxxxxx-xxxx-Cxxx-yxxx-xxxxxxxxxxxx'.split('');
+
+  for (let i = 0; i < idTemplateArray.length; i += 1) {
+    switch (idTemplateArray[i]) {
+      case 'x':
+        idTemplateArray[i] = Math.floor(Math.random() * 16).toString(16);
+        break;
+      case 'y':
+        idTemplateArray[i] = (Math.floor(Math.random() * 4) + 8).toString(16);
+        break;
+      case 'C':
+        idTemplateArray[i] = versionNumber;
+        break;
+    }
+  }
+
+  return idTemplateArray.join('');
+};
