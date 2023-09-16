90df98907e3c740a56573bcfcdc9d1bb648319d5
Ribbit <echo07200503@gmail.com>
2023-09-16 20:39:54 +0900

    👕 kintoneプラグインの設定情報の型を追加

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 export type { kintoneAPI } from './types/api';
+export type * from './types/kintone-config';
+export type * from './types/plugin-config';
 export * from './character-width-conversion';
 export * from './event-listener';
 export * from './plugin';
 export * from './rest-api';
-export * from './types/kintone.config';
+export * from './types/kintone-config';
 export * from './utilities';
 export * from './utility-types';
 export * from './date';
---
+++ src/types/plugin-config.ts
@@ -0,0 +1,66 @@
+type Locales = {
+  /** 日本語 */
+  ja: string;
+  /** 英語 */
+  en: string;
+  /** 簡体字中国語 */
+  zh: string;
+};
+
+type Resources = {
+  /**
+   * プラグインのJavaScriptファイル
+   *
+   * URLの配列
+   */
+  js: string[];
+  /**
+   * プラグインのCSSファイル
+   *
+   * URLの配列
+   */
+  css: string[];
+};
+
+export type PluginManifest = {
+  type: 'APP';
+  manifest_version: 1;
+  version: number | string;
+  /**
+   * プラグインの名前
+   *
+   * インストール時、プラグイン一覧画面で表示されます
+   */
+  name: Locales;
+  /**
+   * プラグインの説明
+   *
+   * インストール時、プラグイン一覧画面で表示されます
+   */
+  description?: Locales;
+  icon: string;
+  homepage_url?: Partial<Locales>;
+  desktop?: Partial<Resources>;
+  mobile?: Partial<Resources>;
+  config?: Partial<Resources> & {
+    /** ファイルの種類 (js/css/html) をキーとする設定画面用カスタマイズファイルと設定情報 */
+    html?: string;
+    /**
+     * 設定画面で設定必須とするパラメータの配列
+     *
+     * ASCIIで1文字以上64文字以下
+     */
+    required_params?: string[];
+  };
+};
+
+export type PluginConfig = {
+  version?: 1;
+  manifest: {
+    base: PluginManifest;
+    dev?: Partial<PluginManifest>;
+    prod?: Partial<PluginManifest>;
+    standalone?: Partial<PluginManifest>;
+  };
+  config_params?: Record<string, any>;
+};
