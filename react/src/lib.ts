c2d8febca0bed9c99e9b0d793b1d3a4d8ae7f177
Ribbit <echo07200503@gmail.com>
2023-11-16 12:03:44 +0900

    🔥 Remove unnecessary error check in embed function

--- react/src/lib.ts
+++ react/src/lib.ts
@@ -3,9 +3,6 @@ import { createRoot } from 'react-dom/client';
 
 export const embed = (component: ReactNode, params: { elementId: string; target: HTMLElement }) => {
   const { elementId, target } = params;
-  if (!target) {
-    throw new Error('Reactコンポーネントの埋め込み先が指定されていません。');
-  }
   if (document.getElementById(elementId)) {
     return;
   }
