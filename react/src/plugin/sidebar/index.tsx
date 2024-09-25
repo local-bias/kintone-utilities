551ed1006cbea6d11b6ea256bef697fd299d43dc
Ribbit <echo07200503@gmail.com>
2024-09-25 09:44:31 +0900

    🚚 ディレクトリを変更

--- react/src/plugin/sidebar/index.tsx
+++ react/src/plugin/sidebar/index.tsx
@@ -1,4 +1,4 @@
-export * from './root';
+export * from './separated/root';
 export * from './separated/ConditionAppendButton';
 export * from './separated/ConditionTab';
 export * from './separated/ConditionTabs';
