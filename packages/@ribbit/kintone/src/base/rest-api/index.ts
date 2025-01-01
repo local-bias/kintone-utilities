3a5b69c40e98149709c0dd3c300608bb76ef3e86
Ribbit <echo07200503@gmail.com>
2025-01-01 14:38:37 +0900

    🚚 プロジェクトのテンプレートをk2へ移行

--- packages/@ribbit/kintone/src/base/rest-api/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './app';
-export * from './file';
-export * from './record';
-export * from './record-comment';
-export * from './report';
-export * from './space';
-export * from './utility';
