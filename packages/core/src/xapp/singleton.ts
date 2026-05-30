0c7682ba59d7fca25d2c63badda01722338f37e2
adachi <adachi@konomi.app>
2026-05-30 11:49:53 +0900

    ✨ キャッシュ機能を追加

--- packages/core/src/xapp/singleton.ts
+++ packages/core/src/xapp/singleton.ts
@@ -1,6 +1,30 @@
 import { getAppObject } from './xapp';
 
+type AppObject = ReturnType<typeof getAppObject>;
+
+let cachedAppObject: AppObject | null = null;
+
+const getResolvedAppObject = (): AppObject => {
+	cachedAppObject ??= getAppObject();
+	return cachedAppObject;
+};
+
+const createAppProxy = (): AppObject =>
+	new Proxy(Object.create(null) as AppObject, {
+		get(_target, propertyKey, receiver) {
+			const appObject = getResolvedAppObject();
+			const value = Reflect.get(appObject, propertyKey, receiver);
+			return typeof value === 'function' ? value.bind(appObject) : value;
+		},
+		set(_target, propertyKey, value, receiver) {
+			return Reflect.set(getResolvedAppObject(), propertyKey, value, receiver);
+		},
+		has(_target, propertyKey) {
+			return propertyKey in getResolvedAppObject();
+		},
+	});
+
 /**
  * kintone javascript APIのルートオブジェクト
  */
-export const xapp = getAppObject();
+export const xapp = createAppProxy();
